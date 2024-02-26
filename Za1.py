92db91b00475c0fd080fd7e4f065941798330a3c
diamotim <diamo_tim@mail.ru>
2024-02-26 17:37:54 +0300

    Zadan1 Working WIP

--- Za1.py
+++ Za1.py
@@ -6,15 +6,16 @@ def PribZnach(n,m,t,xd,yd):
     return [x,y]
 
 
-file=open('space.txt', encoding='UTF-8').read()
-data=file.split('/n')    #Открываем файл и разделяем по рядам
+file=open('space.txt', encoding='UTF-8').read().strip()
+data=file.split('\n')    #Открываем файл и разделяем по рядам
 
 header=data.pop(0).split('*')   #Первая строка с названиями стобцов
-spacenew=[]
-
+space_new=open('space_new.txt','w',encoding='UTF-8')
+space_new.write(str(header).replace('[','').replace(']','').replace('"','').replace("'",'').replace(", ",'*')+'\n')
 for i in data:
     s=i.split('*')
     if s[2]=='0 0':
-        s[2]=PribZnach(int(s[0][5]),int(s[0][6]),len(str(s[1])),int(s[3][0:s[3].find(' ')]),int(s[3][s[3].find(' '):]))
-    spacenew.append(s)
-print(spacenew)
+        s[2]=str(PribZnach(int(s[0][5]),int(s[0][6]),len(str(s[1])),int(s[3][0:s[3].find(' ')]),int(s[3][s[3].find(' '):]))).replace(', ',' ')
+    space_new.write(str(str(s).replace('[','').replace(']','').split(', ')).replace('[','').replace(']','').replace('"','').replace("'",'').replace(", ",'*')+'\n')
+    
+space_new.close()
