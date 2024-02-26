4be8c0cacf3f6f8b619f4ae34a0112f8ce0ece60
diamotim <diamo_tim@mail.ru>
2024-02-26 16:31:45 +0300

    zadan1 WIP

--- Za1.py
+++ Za1.py
@@ -3,3 +3,18 @@ def PribZnach(n,m,t,xd,yd):
     if n<=5: x=-(n+xd)*4+t
     if m>3: y=m+t+yd
     if m<=3: y=-(n+yd)*m
+    return [x,y]
+
+import csv                      #Импортируем библиотеку для работы с таблицой
+
+file=open('space.txt', encoding='UTF-8')
+data=file.read().split('/n')    #Открываем файл и разделяем по рядам
+
+header=data.pop(0).split('*')   #Первая строка с названиями стобцов
+spacenew=open('spacenew.csv','w')
+writer=csv.writer(spacenew)
+writer.writerow(header)
+print(data)
+for i in data:
+    s=i.split('*')
+    print(s)
