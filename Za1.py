1cc379daa2f4ecdcb8639a8a378afca38c7f944c
diamotim <diamo_tim@mail.ru>
2024-02-26 17:54:42 +0300

    Comments and zd2 creation

--- Za1.py
+++ Za1.py
@@ -1,3 +1,4 @@
+#Создаем функцию для нахождения приблизительных координат корабля
 def PribZnach(n,m,t,xd,yd):
     if n>5: x=n+xd
     if n<=5: x=-(n+xd)*4+t
@@ -11,11 +12,14 @@ data=file.split('\n')    #Открываем файл и разделяем по
 
 header=data.pop(0).split('*')   #Первая строка с названиями стобцов
 space_new=open('space_new.txt','w',encoding='UTF-8')
-space_new.write(str(header).replace('[','').replace(']','').replace('"','').replace("'",'').replace(", ",'*')+'\n')
+space_new.write(str(header).replace('[','').replace(']','').replace('"','').replace("'",'').replace(", ",'*')+'\n') #Создаем файл и записываем названия столбцов как первую строчку
+
+#Перебераем строки из файла space.txt
 for i in data:
     s=i.split('*')
     if s[2]=='0 0':
-        s[2]=str(PribZnach(int(s[0][5]),int(s[0][6]),len(str(s[1])),int(s[3][0:s[3].find(' ')]),int(s[3][s[3].find(' '):]))).replace(', ',' ')
-    space_new.write(str(str(s).replace('[','').replace(']','').split(', ')).replace('[','').replace(']','').replace('"','').replace("'",'').replace(", ",'*')+'\n')
-    if s[0][3]=='V': print(s[0]+' - '+'('+str(s[2][:s[2].find(' ')]).replace('[','').replace(']','')+str(s[2][s[2].find(' '):]).replace('[','').replace(']','')+')')
-space_new.close()
+        s[2]=str(PribZnach(int(s[0][5]),int(s[0][6]),len(str(s[1])),int(s[3][0:s[3].find(' ')]),int(s[3][s[3].find(' '):]))).replace(', ',' ') # Изменяем 3 столбик если координаты корабля равны "0 0"
+    space_new.write(str(str(s).replace('[','').replace(']','').split(', ')).replace('[','').replace(']','').replace('"','').replace("'",'').replace(", ",'*')+'\n') #Записываем в файл
+    if s[0][3]=='V': print(s[0]+' - '+'('+str(s[2][:s[2].find(' ')]).replace('[','').replace(']','')+str(s[2][s[2].find(' '):]).replace('[','').replace(']','')+')') #Выводим название корабля вместе с его координатами
+space_new.close() #Закрываем файл для редактирования
+
